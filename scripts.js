fe50dc6847d8144b664de839081e80d04d4c748c
vitoorgaleego12 <vitoorgaleego12@GMAIL.COM>
2024-09-10 02:10:56 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Seleciona o botão de alternância de tema
+const themeToggleButton = document.getElementById('themeToggle');
+
+// Adiciona um evento de clique ao botão
+themeToggleButton.addEventListener('click', toggleTheme);
